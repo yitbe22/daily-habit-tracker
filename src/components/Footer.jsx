6236e57a3431fea02726adbb020aeaf42d36dee5
Yitbarek Tesfaye <yitbarektesfaye22@gmail.com>
2026-09-02 11:45:18 +0300

    Added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+  return (
+    <footer className="bottom-0 w-full bg-slate-950">
+        <div className="max-w-7xl text-white flex items-center justify-between py-3 px-4">
+           <div className="flex space-x-1">
+              <h2 className="text-xs text-orange-400">StreakUp</h2>
+               <p className="text-xs text-gray-300">Build better habits, one day at a time.</p>
+           </div>
+           <div>
+              <p className="text-xs text-gray-300">&copy; 2026 StreakUp</p>
+           </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
